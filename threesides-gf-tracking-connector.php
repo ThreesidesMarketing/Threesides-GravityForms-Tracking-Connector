284028b1cf357387797906c5baf80286eb43e40d
Mathias Everson <39363442+mathias-threesides@users.noreply.github.com>
2024-12-20 10:54:38 +1100

    Fix file name includes
    * Version bump
    * File Name Updates

--- threesides-gf-tracking-connector.php
+++ threesides-gf-tracking-connector.php
@@ -4,14 +4,14 @@
  * Plugin Name: Threesides Gravity Forms Tracking Connector
  * Plugin URI: https://github.com/ThreesidesMarketing/threesides-ga4-gravityforms
  * Description: Plugin for Gravity Forms that adds a hidden fields allowing you to collect relevant tracking data for offline conversion tracking. 
- * Version: 0.0.2
+ * Version: 0.0.4
  * Author: Threesides Marketing
  * Author URI: https://threesides.com.au
  * License: MIT
  * Text Domain: threesides-gf-tracking-connector
  */
 
-define('Threesides_GravityForms_TrackingConnector_Version', '0.0.2');
+define('Threesides_GravityForms_TrackingConnector_Version', '0.0.4');
 add_action('gform_loaded', array('ThreesidesGravityFormsTrackingConnector_Bootstrap', 'load'), 5);
 
 class ThreesidesGravityFormsTrackingConnector_Bootstrap
